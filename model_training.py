50518b7b2cfd45d57706aaf8091847e8d97950be
romulo <romulomanzano@gmail.com>
2019-07-29 22:53:04 -0400

    Changes to model framework

--- model_training.py
+++ model_training.py
@@ -119,7 +119,8 @@ def train_model_from_npy(metadata_location,
                 n_classes=10,
                 training_folds = [10,2,3,4,5,6,7,8],
                 validation_folds = [9],
-                shuffle=True):
+                shuffle=True,
+                epochs = 10):
     """
     """
     params = {'dim': data_dim,
@@ -143,6 +144,7 @@ def train_model_from_npy(metadata_location,
     if model_architecture:
         model = model_architecture
     else:
+        """
         model = Sequential()
         model.add(Conv2D(24, (5,5),
                             data_format='channels_last',
@@ -159,12 +161,34 @@ def train_model_from_npy(metadata_location,
                        kernel_regularizer=regularizers.l2(0.001)))
         # Compile model
         model.compile(loss='categorical_crossentropy', optimizer=optimizers.rmsprop(lr=0.0005, decay=1e-3), metrics=['accuracy'])
+        """
+        model = Sequential()
+        model.add(Conv2D(32, (5, 5), padding='same',
+                 activation='relu',
+                 input_shape=(data_dim[0],data_dim[1],1)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(64, (5, 5),activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, (3, 3), padding='same',activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        model.add(Conv2D(128, (3, 3), padding='same',activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(128, (3, 3),activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(512,activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(n_classes, activation='softmax'))
+        model.compile(optimizers.rmsprop(lr=0.0005, decay=1e-3),loss="categorical_crossentropy",metrics=["accuracy"])
     print(model.summary())
     # Train model on dataset
     steps_per_epoch = np.ceil(len(metadata) / batch_size)
     validation_steps = np.ceil(len(list(test_data['classID']))/batch_size)
     # checkpoint
-    filepath="./keras_checkpoints/npy-weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+    filepath="./keras_checkpoints/npy-weights-improvement-validationfold{}-".format("-".join([str(x) for x in validation_folds]))+"{epoch:02d}-{val_acc:.2f}.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint]
     model.fit_generator(generator=training_generator,
@@ -174,12 +198,12 @@ def train_model_from_npy(metadata_location,
                         verbose=1,
                         steps_per_epoch=steps_per_epoch,
                         validation_steps=validation_steps,
-                        epochs=10,
+                        epochs=epochs,
                         callbacks=callbacks_list,
                         shuffle=True
                        )
     #Validate
-    validation_generator_2 = NumpyDataGenerator(list(test_data['fsID']), labels, id_to_file_mapping, **params)
+    validation_generator_2 = NumpyDataGenerator(list(test_data['fsID']),labels, id_to_file_mapping,file_base_location, **params)
 
     predictions = model.predict_generator(validation_generator_2,
                                       steps = validation_steps,
@@ -192,12 +216,39 @@ def train_model_from_npy(metadata_location,
     print(np.unique(np.array(test_data['classID']), return_counts=True))
 
 
+def train_npy_all_folds(metadata_location,
+                file_base_location,
+                model_architecture=None,
+                data_dim=(128,128),
+                batch_size=64,
+                n_classes=10,
+                shuffle=True):
+    for i in range(1,10):
+        validation_folds = [i]
+        training_folds = set([i for i in range(1,10)]) - set(validation_folds)
+        train_model_from_npy(metadata_location,
+                        file_base_location,
+                        model_architecture=model_architecture,
+                        data_dim=data_dim,
+                        batch_size=batch_size,
+                        n_classes=n_classes,
+                        training_folds = training_folds,
+                        validation_folds = validation_folds,
+                        shuffle=True)
+
+
+
+
 if __name__ == "__main__":
     model_run = sys.argv[1]
     if model_run == "npy":
         metadata_location = sys.argv[2]
         file_base_location = sys.argv[3]
         train_model_from_npy(metadata_location, file_base_location)
+    elif model_run == "npy_all_folds":
+        metadata_location = sys.argv[2]
+        file_base_location = sys.argv[3]
+        train_npy_all_folds(metadata_location, file_base_location)
     elif model_run == "png":
         metadata_location = sys.argv[2]
         file_base_location = sys.argv[3]
